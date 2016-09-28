a7c776629aa8f6fe0e3d8ebb16eeda50cbc40b02
n3vrax <n3vrax@gmail.com>
2016-09-28 19:40:06 +0300

    more exception classes

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright: DotKernel
+ * @library: dotkernel/dot-authentication
+ * @author: n3vrax
+ * Date: 9/6/2016
+ * Time: 7:49 PM
+ */
+
+namespace Dot\Authentication\Exception;
+
+/**
+ * Class InvalidArgumentException
+ * @package Dot\Authentication\Exception
+ */
+class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
+{
+
+}
---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright: DotKernel
+ * @library: dotkernel/dot-authentication
+ * @author: n3vrax
+ * Date: 9/6/2016
+ * Time: 7:49 PM
+ */
+
+namespace Dot\Authentication\Exception;
+
+/**
+ * Class RuntimeException
+ * @package Dot\Authentication\Exception
+ */
+class RuntimeException extends \RuntimeException implements ExceptionInterface
+{
+
+}
